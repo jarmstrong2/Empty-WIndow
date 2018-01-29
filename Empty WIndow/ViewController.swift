8a239658f7464c53c0cda71adf3dc609bc3934f4
John Armstrong <j2armstrong@gmail.com>
2018-01-29 14:18:36 -0500

    ok

--- Empty WIndow/ViewController.swift
+++ Empty WIndow/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     /**
-     Many people would like to dog their cats. So it is *perfectly*
+     Many people woulkd like to dog their cats. So it is *perfectly*
      reasonable to supply a convenience method to do so:
      * Because it's cool.
      * Because it's there.
